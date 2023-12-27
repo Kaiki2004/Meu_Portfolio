8def1cfa32be40e281a3b76d84368a507302248c
Kaiki Andrade <kaikiandrade64@gmail.com>
2023-12-27 15:17:17 -0300

    alterei o arquivo

--- portfolio/animacao.js
+++
@@ -1,5 +0,0 @@
-new TypeIt("#animacao", {
-    strings: ["Bem-vindo!", "Sou o Kaiki Andrade, estudande de Engenharia de Software e esse é o meu portfólio.", "Aqui você encontrará tudo sobre mim, minha trajetória, meus projetos e mais."],
-    speed: 20,
-    waitUntilVisible: true,
-  }).go();
