faeda841781b68af728e38fa8ea3c48b1d58ede8
Kaiki Andrade <kaikiandrade64@gmail.com>
2023-12-29 15:22:26 -0300

    ajustes

--- portfolio/animacao.js
+++ portfolio/animacao.js
@@ -1,5 +1,7 @@
 new TypeIt("#animacao", {
-    strings: ["Bem-vindo!", "Sou o Kaiki Andrade, estudande de Engenharia de Software e esse é o meu portfólio.", "Aqui você encontrará tudo sobre mim, minha trajetória, meus projetos e mais."],
-    speed: 25,
-    waitUntilVisible: true,
-  }).go();
+  strings: ["Bem-vindo!", "Sou o Kaiki Andrade, estudande de Engenharia de Software e esse é o meu portfólio.", "Aqui você encontrará tudo sobre mim, minha trajetória, meus projetos e mais"],
+  speed: 25,
+  waitUntilVisible: true,
+})
+.type("veja mais")
+.go();
